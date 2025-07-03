03193bf72784f2fdd78be95a0ee5b99746960ebc
AdenilsonNegri <adenilsonegri@gmail.com>
2025-07-02 23:51:02 -0300

    incluido o botão ‘Alterar’ junto ao botão ‘Excluir’

--- Exercicio_3/Cadastro.js
+++ Exercicio_3/Cadastro.js
@@ -1,6 +1,8 @@
 const form = document.getElementById("formCelular");
 const btnSalvar = document.getElementById("btnSalvar");
 
+let indiceEdicao = localStorage.getItem("celularParaEditar");
+
 function validarCampos() {
   const marca = document.getElementById("marca").value;
   const modelo = document.getElementById("modelo").value.trim();
@@ -34,11 +36,36 @@ form.addEventListener("submit", function (e) {
 
   // Salvar no localStorage
   const lista = JSON.parse(localStorage.getItem("celulares")) || [];
-  lista.push(celular);
+
+  if (indiceEdicao !== null) {
+    // Atualizar
+    lista[indiceEdicao] = celular;
+    localStorage.removeItem("celularParaEditar");
+  } else {
+    // Novo Cadastro
+    lista.push(celular);
+  } 
+ 
   localStorage.setItem("celulares", JSON.stringify(lista));
 
   alert("Dados salvos com sucesso!");
 
   form.reset();
   btnSalvar.disabled = true;
+  window.location.href = "Listagem_Celulares_3.html";
 });
+// Pré-preencher se estiver editando
+if (indiceEdicao !== null) {
+  const celulares = JSON.parse(localStorage.getItem("celulares")) || [];
+  const celular = celulares[indiceEdicao];
+
+  if (celular) {
+    document.getElementById("marca").value = celular.marca;
+    document.getElementById("modelo").value = celular.modelo;
+    document.getElementById("cor").value = celular.cor;
+    document.getElementById("valor").value = celular.valor;
+    document.querySelector(`input[name="estado"][value="${celular.estado}"]`).checked = true;
+    document.getElementById("info").value = celular.info;
+    validarCampos();
+  }
+}
